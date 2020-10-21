e6d869de93ce5460a02af01b5d167d90ab179540
LPisano721 <66844038+LPisano721@users.noreply.github.com>
2020-10-21 11:26:42 -0400

    Add files via upload
    # panel_demo
    
    This program features asimple guessing game that demonstrates IF, ELIF, and ELSE statements as well as using a counter system. This program is using breezypythongui.py to assist with creating a GUI.
    
    # Note:
    Both the files, paneldemo.py and breezypythongui.py, must be in the same working directory

---
+++ numberguess.py
@@ -0,0 +1,53 @@
+"""
+Program: CounterDemo.py
+Author: Luigi Pisano 10/16/20
+case study on 269-273
+
+GUI-based version of a number guessing game. Game will indicate if the user's guess is too small, too large or when they win. Also indicate the number of attempts before a win.
+"""
+
+from breezypythongui import EasyFrame
+import random
+class GuessingGame(EasyFrame):
+	"""Plays a guessing game with the user."""
+	def __init__(self):
+		"""Sets up the window and the widgets."""
+		EasyFrame.__init__(self, title = "Guessing Game")
+		
+		# Initialize the instance variables for the data
+		self.myNumber = random.randint(1, 10)
+		self.count = 0
+		# Create and add widgets to the window
+		greeting = "Guess a number between 1 and 100."
+		self.hintLabel = self. addLabel(text = greeting, row = 0, column = 0, sticky = "NSEW", columnspan = 2)
+		self.addLabel(text = "Your guess:", row = 1, column = 0)
+		self.guessField = self.addIntegerField(value = 0, row = 1, column = 1)
+		# Buttons have no command attributes yet
+		self.nextButton = self.addButton(text = "Guess", row = 2, column = 0, command = self.nextGuess)
+		self.newButton = self.addButton(text = "New Game", row = 2, column = 1, command = self.newGame)
+	# Event handling functions
+	def nextGuess(self):
+		"""Processes the user's next guess."""
+		self.count += 1
+		guess = self.guessField.getNumber()
+		# Logic to determine the game result
+		if guess == self.myNumber:
+			self.hintLabel["text"] = "You won! you've guessed it in " + str(self.count) + " attempts!"
+			self.nextButton["state"] = "disabled"
+		elif guess < self.myNumber:
+			self.hintLabel["text"] = "Sorry, your guess was too low."
+		else:
+			self.hintLabel["text"] = "Whoops, your guess was too high."
+	def newGame(self):
+		"""Resets the game data and GUI to their original states."""
+		self.myNumber = random.randint(1, 100)
+		self.count = 0
+		greeting = "Guess a number between 1 and 100"
+		self.hintLabel["text"] = greeting
+		self.guessField.setNumber(0)
+		self.nextButton["state"] = "normal"
+#Definition of the main() function
+def main():
+	GuessingGame().mainloop()
+# Global call to the main() function
+main()
